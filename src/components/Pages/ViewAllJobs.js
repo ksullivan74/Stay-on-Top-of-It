2332088eb3d9998941e0f9ac68227e0fbaf1afd5
Neil Sullivan <ac30delay@gmail.com>
2023-02-06 16:05:10 -0600

    delete job function complete

--- src/components/Pages/ViewAllJobs.js
+++ src/components/Pages/ViewAllJobs.js
@@ -4,20 +4,75 @@ import "../Pages/Pages.css"
 
 export const ViewAllJobs = () => {
     const [jobs, setJobs] = useState([])
+    const [users, getUsers] = useState([])
 
-    const navigate = useNavigate()
+    const localStayUser = localStorage.getItem("stay_user")
+    const stayUserObject = JSON.parse(localStayUser)
 
-    useEffect(
-        () => {
-            fetch(`http://localhost:8088/jobs/?_expand=cadence&_expand=category`)
+    const getAllJobs = () => {
+        fetch(`http://localhost:8088/jobs/?_expand=cadence&_expand=category`)
                 .then(resp => resp.json())
                 .then((jobsArray) => {
                     setJobs(jobsArray)
                 }
                 )
+    }
+    
+    useEffect(
+        () => {
+            getAllJobs()
         }, []
     )
 
+    useEffect(() => {
+        fetch(`http://localhost:8088/users`)
+        .then( resp => resp.json())
+        .then ((userArray) => {
+            getUsers(userArray)
+        })
+    },[]
+    )
+
+    const userHelperMatch = (helperIdObj) =>{
+
+       /* const matchingHelper = users.find(
+            (user) => {
+                return  user.id === helperIdObj
+            }
+            )
+            return `${matchingHelper.name}`
+        */
+
+        let matchingHelper = ""
+
+        for ( const user of users) {
+            if (user.id === helperIdObj) {
+                matchingHelper = user.name
+            }
+        }
+        return matchingHelper
+    }
+
+    const userForemanMatch = (helperIdObj) =>{
+
+        /* const matchingHelper = users.find(
+             (user) => {
+                 return  user.id === helperIdObj
+             }
+             )
+             return `${matchingHelper.name}`
+         */
+ 
+         let matchingForeman = ""
+ 
+         for ( const user of users) {
+             if (user.id === helperIdObj) {
+                 matchingForeman = user.name
+             }
+         }
+         return matchingForeman
+     }
+    
 
   return <>
   <h2>Jobs:</h2>
@@ -28,10 +83,12 @@ export const ViewAllJobs = () => {
             if (job.compeleteDate === null) {
                 return <>
                     <article className="jobElement">
-                            <header className="jobLine">Job: {job.title}</header>
+                            <header  className="jobLine">Job: {job.title}</header>
                             <header  className="jobLine">Due Date: {job.dueDate}</header>
                             <header  className="jobLine">Occurs: {job.cadence.cadence}</header>
                             <header  className="jobLine">Category: {job.category.category}</header>
+                            <header  className="jobLine">Assigned To: {userHelperMatch(job.helperId)}</header>
+                            <header  className="jobLine">Assigned By: {userForemanMatch(job.foremanId)}</header>
                             <header  className="jobLine">Is it Done? Click this button:</header>
                             <button
                                 onClick={() => {
@@ -46,7 +103,7 @@ export const ViewAllJobs = () => {
                                         body: JSON.stringify(dateUpdate)
                                     })
                                         .then(() => {
-                                            navigate("/ViewAllJobs")
+                                            getAllJobs()
                                         })
                                 }} className="button">
                                 Complete Job
